b2b7f3c00aa5fba5148d07bbd5082fd3ae1ee590
Carlos Adryan <carlosadryana@gmail.com>
2025-05-02 14:09:47 -0300

    Simple query with query methods

--- src/main/java/com/adryancarlos/workshopmongo/services/PostService.java
+++ src/main/java/com/adryancarlos/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.adryancarlos.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.adryancarlos.workshopmongo.domain.Post;
-import com.adryancarlos.workshopmongo.domain.User;
 import com.adryancarlos.workshopmongo.repository.PostRepository;
 import com.adryancarlos.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -19,5 +19,9 @@ public class PostService {
 	public Post findById(String id) {
 	    Optional<Post> user = repo.findById(id);
 	    return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
